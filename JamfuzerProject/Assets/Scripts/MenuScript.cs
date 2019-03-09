b585df9de4fe4912803d794cae72420bd539ef0a
Elliot Chester <elliotchester@outlook.com>
2019-03-09 14:43:32 +0000

    Added menus

---
+++ JamfuzerProject/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour
+{
+    public void OpenMenu(GameObject MenuToOpen)
+    {
+        MenuToOpen.SetActive(true);
+    }
+
+    public void CloseMenu(GameObject MenuToClose)
+    {
+        MenuToClose.SetActive(false);
+    }
+}
